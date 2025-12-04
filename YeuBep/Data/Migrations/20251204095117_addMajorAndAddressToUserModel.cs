593e40b3c3a1cbadd9ec4e79773dae1d49f8e6fa
= <ninhlk@histaff.vn>
2025-12-04 21:31:46 +0700

    update

---
+++ YeuBep/Data/Migrations/20251204095117_addMajorAndAddressToUserModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YeuBep.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addMajorAndAddressToUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Majors",
+                table: "AspNetUsers",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Majors",
+                table: "AspNetUsers");
+        }
+    }
+}
